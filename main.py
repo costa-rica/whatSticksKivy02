f2098348e481c93b88066c59627cf3e5b715a5bf
costa-rica <nrodrig1@gmail.com>
2021-11-17 10:03:25 -0500

    functionality update: app runs, fixed issue with passing email from login screen to ps2 to activity screen. Fix was to move childScreenManger out of MainApp into its own class to add acitivty_screen and table_screen. Then instantiate the new ChildScreenManger class inside ParentScreen2(ps2).

--- main.py
+++ main.py
@@ -50,11 +50,12 @@ class ParentScreen1(Screen):
                 if i['email']==self.email.text:
 
                     ActivityScreen.user_name_str=i['username']
-                    ActivityScreen.user_timezone=i['user_timezone']
+                    ParentScreen2.user_timezone=i['user_timezone']
+                    # print('i[user_timezone;;;;',i['user_timezone'])
                     ActivityScreen.user_id_str=i['id']
-                    ActivityScreen.user_email=self.email.text
-                    ActivityScreen.user_password=self.password.text
-                    # self.reset()
+                    ParentScreen2.email=self.email.text
+                    ActivityScreen.email=self.email.text
+                    print('ParentScreen1 login_button email:::',self.email.text)
 
                     #sm.current = 'main'
                     self.app.psm.current="ps2"
@@ -74,74 +75,46 @@ class CanvasWidget(Widget):
             self.rect.size = self.size
 
 class ParentScreen2(Screen):
-    # csm=ObjectProperty(None)
+    user_timezone=''
+    email=''
     def __init__(self,**kwargs):
         super(ParentScreen2, self).__init__(**kwargs)
         self.super_box=BoxLayout(orientation ='vertical')
-#screenmanger csm
-
-        # self.csm=ScreenManager()
-        # activity_screen=ActivityScreen(name="activity_screen")
-        # table_screen=TableScreen(name="table_screen")
-        # self.csm.add_widget(activity_screen)
-        # self.csm.add_widget(table_screen)
-        # self.csm.current="activity_screen"
-        # self.csm=csm
-#end screenmanger csm
-#Add navigation slide out thingy
+    #Add navigation slide out thingy
         nav_drawer=MDNavigationDrawer()
         self.nav_drawer=nav_drawer
-#navigation menu
         nav_box=NavMenu()
-        # nav_box=BoxLayout(orientation ='vertical',size_hint=(0.1,0.9))
-        # nav_button1 = Button(text="Button(Nav) go to childS2")
-        # nav_button1.bind(on_press=self.nav_change_screen1)
-        # nav_button2 = Button(text="Button(Nav) go to childS1")
-        # nav_button2.bind(on_press=self.nav_change_screen2)
-        # nav_box.add_widget(nav_button1)
-        # nav_box.add_widget(nav_button2)
-#end nav menu
-
         nav_drawer.add_widget(nav_box)
-        # self.super_box.add_widget(self.csm)
         toolbar=Toolbar()
         toolbar.left_action_items=[["menu", lambda x: nav_drawer.set_state("open")]]
         screenbackground=CanvasWidget()
-        #order of self widgets matters:
+    #order of self widgets matters:
         self.add_widget(screenbackground,index=99)
         self.add_widget(self.super_box)
         self.add_widget(nav_drawer)
         self.add_widget(toolbar)
 
     def on_enter(self):
-        self.app=MainApp.get_running_app()
-        self.csm=self.app.csm
+        ActivityScreen.user_timezone=self.user_timezone
+        ActivityScreen.email=self.email
+        self.csm=ChildScreenManger()
         self.super_box.add_widget(self.csm)
 
-    # def nav_change_screen1(self,instance):
-    #     print('inside nav_change_screen1')
-    #     self.csm.current="table_screen"
-    #
-    # def nav_change_screen2(self,instance):
-    #     print('inside nav_change_screen2')
-    #     self.csm.current="activity_screen"
-
 class ActivityScreen(Screen):
-    email=ObjectProperty(None)
+    email=''
+    email_label=ObjectProperty()
     time_thing=ObjectProperty()
     date_thing=ObjectProperty()
-    user_timezone='Europe/Paris'
+    user_timezone=''
+    # user_timezone='Europe/Paris'#'US/Eastern'
     def __init__(self,**kwargs):
         super(ActivityScreen,self).__init__(**kwargs)
-
         self.app=MainApp.get_running_app()
         self.date_time_obj=datetime.datetime.now()
-        print('initialized Activity Screen - what is email::', self.email.text)
-        print('initialized Activity Screen - what is user_timezone::', self.user_timezone)
 
     def on_enter(self,*args):
-        print('are we ok here????')
-        # self.email.text=self.user_name_str
+        print('Activity Screen on_enter email:::',self.email_label.text)
+        self.email_label.text=self.email
         self.date_time_now=current_time_util(self.user_timezone)
         self.ids.date_thing.text=self.date_time_now[0]
         self.ids.time_thing.text=self.date_time_now[1]
@@ -178,19 +151,17 @@ class NavMenu(BoxLayout):
         nav_box.add_widget(nav_button1)
         nav_box.add_widget(nav_button2)
         self.app=MainApp.get_running_app()
-        # self.ps2=ParentScreen2()
-        # self.csm=self.app.csm
-        # self.csm=self.ps2.csm
 
 
     def on_entry(self):
         print('********when does NavMenu on_entry fire?******')
+        # self.csm=self.app.ps2.csm
 
     def nav_change_screen1(self):
-        self.app.csm.current="table_screen"
+        self.app.ps2.csm.current="table_screen"
 
     def nav_change_screen2(self):
-        self.app.csm.current="activity_screen"
+        self.app.ps2.csm.current="activity_screen"
 
 
 class Toolbar(MDToolbar):...
@@ -199,24 +170,25 @@ class ConfirmBox(BoxLayout):...
 
 class FailBox(BoxLayout):...
 
-class MainApp(MDApp):
-    def build(self):
-        # print('MDTextField:::',dir(MDTextField()))
-        ps1=ParentScreen1(name='ps1')
-        ps2=ParentScreen2(name='ps2')
-        psm=ScreenManager()
-        self.psm=psm
-        psm.add_widget(ps1)
-        psm.add_widget(ps2)
-        self.icon = "icon.png"
-
-        csm=ScreenManager()
+class ChildScreenManger(ScreenManager):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
         activity_screen=ActivityScreen(name="activity_screen")
         table_screen=TableScreen(name="table_screen")
-        csm.add_widget(activity_screen)
-        csm.add_widget(table_screen)
-        csm.current="activity_screen"
-        self.csm=csm
+        self.add_widget(activity_screen)
+        self.add_widget(table_screen)
+        self.current="activity_screen"
+
+class MainApp(MDApp):
+    def build(self):
+        self.ps1=ParentScreen1(name='ps1')
+        self.ps2=ParentScreen2(name='ps2')
+        psm=ScreenManager()
+        self.psm=psm
+        psm.add_widget(self.ps1)
+        psm.add_widget(self.ps2)
+        self.icon = "icon.png"
+
 
         return psm
 
